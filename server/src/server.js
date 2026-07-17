324d8f0305e78c8e8467c22c6c680b29e7a62d7d
faizalkhan0871 <faizalkhan29444@gmail.com>
2026-07-17 17:41:29 +0530

    Fix CORS

--- server/src/server.js
+++ server/src/server.js
@@ -38,21 +38,9 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-const allowedOrigins = [
-  process.env.CLIENT_URL,
-  "http://localhost:3000",
-  "http://localhost:3001",
-].filter(Boolean);
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: true,
     credentials: true,
   })
 );
